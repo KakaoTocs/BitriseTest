7464908794b10016e5d97e9860817a8f24b61995
KakaoTocs <kimjinu5555@gmail.com>
2021-07-13 15:23:32 +0900

    Modify Test code

--- BitriseTestTests/BitriseTestTests.swift
+++ BitriseTestTests/BitriseTestTests.swift
@@ -26,9 +26,6 @@ class BitriseTestTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
