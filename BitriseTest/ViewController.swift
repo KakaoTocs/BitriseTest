1a8f8385de675f2a4f5e29eb7d684ae234b0a71d
kimjinu <kimjinu5555@gmail.com>
2021-07-13 14:50:40 +0900

    Init project

---
+++ BitriseTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitriseTest
+//
+//  Created by GAMZA on 2021/07/13.
+//
+
+import UIKit
+import SDWebImage
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
